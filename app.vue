b303c3626b159de6e433a5b7295f6d36ec514324
amin <asoltani927@gmail.com>
2025-05-15 08:50:37 +0330

    fix titles

--- app.vue
+++ app.vue
@@ -6,15 +6,16 @@
 </template>
 
 <script setup>
-const { locale, locales } = useI18n()
-const currentDir = computed(() =>
-  locales.value.find(l => l.code === locale.value)?.dir || 'ltr'
-)
+const { locale, locales } = useI18n();
+const currentDir = computed(
+  () => locales.value.find((l) => l.code === locale.value)?.dir || "ltr"
+);
 useHead({
+  title: "پی جیب تبادل ارز سریع و امن با کمترین کارمزد",
+  titleTemplate: "%s | پی جیب",
   htmlAttrs: {
-    dir: currentDir
-  }
-})
-
+    dir: currentDir,
+  },
+});
 </script>
 
