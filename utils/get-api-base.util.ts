c0dd090f48d0829e4ff2e96802cdf197faaea2f5
Amin Soltani <asoltani927@gmail.com>
2025-05-24 14:49:15 +0330

    fix issue

--- utils/get-api-base.util.ts
+++ utils/get-api-base.util.ts
@@ -2,7 +2,7 @@ import { isProduction } from "./is-production.util";
 
 export function getApiBase(version: "v1" | "v2" = "v1") {
   if (isProduction()) {
-    return `${process.env.NUXT_PUBLIC_API_BASE || 'https://pjb-staging.darkube.app/api/'}` + version;
+    return `${process.env.NUXT_PUBLIC_API_BASE ?? 'https://pjb-staging.darkube.app/api/'}` + version;
   }
-  return `${process.env.NUXT_PUBLIC_API_BASE || 'https://pjb-staging.darkube.app/api/'}` + version;
+  return `${process.env.NUXT_PUBLIC_API_BASE ?? 'https://pjb-staging.darkube.app/api/'}` + version;
 }
