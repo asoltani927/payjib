a33e61b3517984499b4ac1c38ab191bc840cebdc
Kashefy <kashefymajid1992@gmail.com>
2025-05-26 16:24:27 +0330

    change variables

--- utils/get-api-base.util.ts
+++ utils/get-api-base.util.ts
@@ -3,7 +3,7 @@ import { isProduction } from "./is-production.util";
 export function getApiBase(version: "v1" | "v2" = "v1") {
   console.log(process.env.NUXT_PUBLIC_API_BASE);
   if (isProduction()) {
-    return `${process.env.NUXT_PUBLIC_API_BASE ?? 'https://pjb-staging.darkube.app/api/'}` + version;
+    return `${process.env.NUXT_PUBLIC_API_BASE ?? 'https://service.payjib.com/api/'}` + version;
   }
-  return `${process.env.NUXT_PUBLIC_API_BASE ?? 'https://pjb-staging.darkube.app/api/'}` + version;
+  return `${process.env.NUXT_PUBLIC_API_BASE ?? 'https://service.payjib.com/api/'}` + version;
 }
